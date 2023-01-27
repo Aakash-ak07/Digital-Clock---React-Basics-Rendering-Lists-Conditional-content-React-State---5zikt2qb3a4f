da02a49cc4ca23e121025963a409ac4fa1e0ce69
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-27 22:44:10 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { Component } from "react";
 import '../styles/App.css';
-const App = () => {
 
-  return (
-    <div id="main">
-      <div className="date-time"></div>
-    </div>
-  )
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      time: new Date().toLocaleString()
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleString(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+   render() {
+    return (
+      <div id="main">
+        <div className="date-time">{this.state.time}</div>
+      </div>
+    );
+  }
 }
 
 
